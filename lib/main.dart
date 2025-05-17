13ade23fc8f8ca151a7563a3ea80708044246005
Jon Templin <jon@twinpalmshome.com>
2025-05-16 23:48:12 -0400

    Checkpoint

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 void main() => runApp(CalculatorApp());
 
 class CalculatorApp extends StatelessWidget {
-  const CalculatorApp({super.key}); // Constructor with super.key
+  const CalculatorApp({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -15,8 +17,77 @@ class CalculatorApp extends StatelessWidget {
   }
 }
 
-class CalculatorSkeleton extends StatelessWidget {
-  const CalculatorSkeleton({super.key}); // Constructor with super.key
+class CalculatorSkeleton extends StatefulWidget {
+  const CalculatorSkeleton({super.key});
+
+  @override
+  State<CalculatorSkeleton> createState() => _CalculatorSkeletonState();
+}
+
+class _CalculatorSkeletonState extends State<CalculatorSkeleton> {
+  double Xregister = 0;
+  bool decimalMode = false;
+  int decimalPlaces = 0;
+  final formatter = NumberFormat("#,##0.00");
+
+  @override
+  void initState() {
+    super.initState();
+    Xregister = 0;
+    decimalMode = false;
+    decimalPlaces = 0;
+    // Initialize any state or variables here
+  }
+
+  void onButtonPressed(String label) {
+    switch (label) {
+      case '0':
+      case '1':
+      case '2':
+      case '3':
+      case '4':
+      case '5':
+      case '6':
+      case '7':
+      case '8':
+      case '9':
+        double digit = double.parse(label);
+        if (decimalMode) {
+          decimalPlaces++;
+          double decimal = digit * pow(10, -decimalPlaces).toDouble();
+          Xregister += decimal;
+        } else {
+          Xregister = Xregister * 10 + digit;
+        }
+        break;
+
+      case '.':
+        decimalMode = true;
+        decimalPlaces = 0;
+        break;
+
+      case '+/-':
+        Xregister = -Xregister;
+        break;
+
+      case '+':
+      case '-':
+      case '×':
+      case '÷':
+        //handleOperator(key);
+        break;
+
+      case 'CLX':
+        Xregister = 0;
+        decimalMode = false;
+        break;
+
+      default:
+        print('Unknown key: $label');
+    }
+    String formatted = formatter.format(Xregister);
+    print('X: $formatted');
+  }
 
   static const double buttonHeight = 80;
 
@@ -26,7 +97,7 @@ class CalculatorSkeleton extends StatelessWidget {
         margin: const EdgeInsets.all(4),
         height: buttonHeight,
         child: ElevatedButton(
-          onPressed: () {},
+          onPressed: () => onButtonPressed(label),
           style: ElevatedButton.styleFrom(
             backgroundColor: color ?? Colors.grey[800],
             padding: const EdgeInsets.all(8),
@@ -77,77 +148,92 @@ class CalculatorSkeleton extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.black,
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(12.0),
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              // Left side: 4x3 number pad
-              Expanded(
-                flex: 3,
-                child: Column(
-                  children: [
-                    Row(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  //
+                  // Left side: 4x3 number pad
+                  Expanded(
+                    flex: 3,
+                    child: Column(
                       children: [
-                        buildButton('7'),
-                        buildButton('8'),
-                        buildButton('9'),
-                      ],
-                    ),
-                    Row(
-                      children: [
-                        buildButton('4'),
-                        buildButton('5'),
-                        buildButton('6'),
-                      ],
-                    ),
-                    Row(
-                      children: [
-                        buildButton('1'),
-                        buildButton('2'),
-                        buildButton('3'),
-                      ],
-                    ),
-                    Row(
-                      children: [
-                        buildButton('0'),
-                        buildButton('00'),
-                        buildButton('.'),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
-              const SizedBox(width: 12),
-
-              // Right side
-              Expanded(
-                flex: 2,
-                child: Column(
-                  children: [
-                    Row(children: [buildButton('±'), buildButton('÷')]),
-                    Row(children: [buildButton('%'), buildButton('×')]),
-                    Row(
-                      children: [
-                        Expanded(child: buildVerticalEnterButton('ENTER')),
-                        SizedBox(
-                          height: buttonHeight * 2 + 8,
-                          child: Column(
-                            mainAxisSize: MainAxisSize.max,
-                            children: [buildButton('-'), buildButton('+')],
-                          ),
+                        Row(
+                          children: [
+                            buildButton('7'),
+                            buildButton('8'),
+                            buildButton('9'),
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            buildButton('4'),
+                            buildButton('5'),
+                            buildButton('6'),
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            buildButton('1'),
+                            buildButton('2'),
+                            buildButton('3'),
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            buildButton('0'),
+                            buildButton('.'),
+                            buildButton('CLX'),
+                          ],
                         ),
                       ],
                     ),
-                  ],
-                ),
+                  ),
+                  const SizedBox(width: 12),
+
+                  //
+                  // Right side: 2x4 operator pad
+                  Expanded(
+                    flex: 2,
+                    child: Column(
+                      children: [
+                        Row(children: [buildButton('+/-'), buildButton('÷')]),
+                        Row(children: [buildButton('%'), buildButton('×')]),
+                        Row(
+                          children: [
+                            Expanded(child: buildVerticalEnterButton('ENTER')),
+                            SizedBox(
+                              height: buttonHeight * 2 + 8,
+                              child: Column(
+                                mainAxisSize: MainAxisSize.max,
+                                children: [buildButton('-'), buildButton('+')],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                  //
+                  //
+                ],
               ),
-              //
-              //
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
   }
 }
+
+
+// final formatter = NumberFormat("#,##0.00");
+
+// double value = 1234567.89;
+// String formatted = formatter.format(value);
+
+// print(formatted); // Outputs: 1,234,567.89
