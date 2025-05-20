5dcb31b97247d5bc070a3d02468dbf8921a9bfc0
Jon Templin <jon@twinpalmshome.com>
2025-05-20 13:16:53 -0400

    simplified CalcStack to remove unecessary getters and setters

--- lib/calc_stack.dart
+++ lib/calc_stack.dart
@@ -1,56 +1,45 @@
 import 'package:flutter/foundation.dart';
 
 class CalcStack {
-  double _x = 0.0;
-  double _y = 0.0;
-  double _z = 0.0;
-  double _t = 0.0;
+  double x = 0.0;
+  double y = 0.0;
+  double z = 0.0;
+  double t = 0.0;
 
   CalcStack() {
     clear();
   }
 
-  // Aliases for registers
-  double get x => _x;
-  double get y => _y;
-  double get z => _z;
-  double get t => _t;
-
-  set x(double value) => _x = value;
-  set y(double value) => _y = value;
-  set z(double value) => _z = value;
-  set t(double value) => _t = value;
-
   // Push: X->Y, Y->Z, Z->T, T lost, new X
   void push(double value) {
-    _t = _z;
-    _z = _y;
-    _y = _x;
-    _x = value;
+    t = z;
+    z = y;
+    y = x;
+    x = value;
   }
 
   // Pop: X is popped, Y->X, Z->Y, T->Z, T stays the same
   double pop() {
-    final popped = _x;
-    _x = _y;
-    _y = _z;
-    _z = _t;
+    final popped = x;
+    x = y;
+    y = z;
+    z = t;
     // T remains unchanged (rolls down)
     return popped;
   }
 
   // Clear all registers to zero
   void clear() {
-    _x = 0.0;
-    _y = 0.0;
-    _z = 0.0;
-    _t = 0.0;
+    x = 0.0;
+    y = 0.0;
+    z = 0.0;
+    t = 0.0;
   }
 
   // For debugging
   void dump() {
     if (kDebugMode) {
-      print('T: $_t\nZ: $_z\nY: $_y\nX: $_x');
+      print('T: $t\nZ: $z\nY: $y\nX: $x');
     }
   }
 }
